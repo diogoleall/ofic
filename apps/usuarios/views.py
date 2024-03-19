a0bff0259081f12a64202d1f63ef3ca5cced1d0b
diogoleall <contato.lealdiogo@gmail.com>
2024-03-18 21:26:51 -0300

    18/03/24

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def _login(request):
+    template_name = 'usuarios/html'
+    context ={}
+    return render (request, template_name , context)
+
